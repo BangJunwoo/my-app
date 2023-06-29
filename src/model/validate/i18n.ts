2a1b84e656b4a25c37f967a3736015b8743397c2
Bang Junwoo <bang.jun@sidewalkplay.com>
2023-06-29 19:36:21 +0900

    i18n test

---
+++ src/model/validate/i18n.ts
@@ -0,0 +1,7 @@
+import * as ko from './ko'
+
+console.log(ko)
+
+export const message = {
+  required_error: '내용을 채워주세요',
+}
---
+++ src/model/validate/ko.js
@@ -0,0 +1,2 @@
+export const nameRequired_error = 'abcd'
+export const nameMin_error = 'min error'
