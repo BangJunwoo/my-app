fe35886815f350bb6a2f56944b79c3634e72470a
Bang Junwoo <bang.jun@sidewalkplay.com>
2023-07-17 10:46:32 +0900

    센트리 추가

--- next.config.js
+++ next.config.js
@@ -7,4 +7,27 @@ const nextConfig = {
   },
 }
 
+// Injected content via Sentry wizard below
+
+const { withSentryConfig } = require('@sentry/nextjs')
+
+const sentryWebpackPluginOptions = {
+  org: 'sidewalk-entertainment',
+  project: 'sidewalkplay-nextjs',
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+
+  silent: true,
+
+  widenClientFileUpload: true,
+
+  transpileClientSDK: true,
+
+  tunnelRoute: '/monitoring',
+
+  hideSourceMaps: true,
+
+  disableLogger: true,
+}
+
 module.exports = nextConfig
+// module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions)
